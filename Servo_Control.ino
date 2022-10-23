fec12ed9f91f24017f35c565648a814e9e70ce08
Goli Vishnuvardhan Reddy <104411008+vishnuvardhanreddy31@users.noreply.github.com>
2022-10-23 15:03:00 +0530

    Update Servo_Control.ino

--- Servo_Control.ino
+++ Servo_Control.ino
@@ -1,19 +1,3 @@
-/*
- * Servo Control using Blynk and ESP8266 (Nodemcu)
- * The project controls the position of a servo motor using Blynk apps. I'm using an SG90 180 degree micro servo. Nodemcu board can drive this servo motor without an additional power supply.
- * 
- * How its work?
- * - Slide the slider widget to the left or right (Minimum value 0 degrees, Maximum Value 180 degrees).
- * - The servo motor will position according to the slider value.
- * 
- * The circuit:
- * - Connect servo motor GND (brown wire), VCC (red wire), and DATA (orange wire) pins to the GROUND, 3V/5V, and D2 (GPIO 4) of the Nodemcu.
- * - Create a datastream (Virtual Pin) and name them SERVO. Select PIN to V0; select Data Type to Int; set MIN 0, MAX 180, Default value 0.
- * - Create slider widgets from the Blynk web dashboard and mobile dashboard. Go to slider settings and select datastream SERVO (V0).
- * 
- * Edgent_ESP8266 & Servo from Blynk <https://github.com/blynkkk/blynk-library/releases/latest>
- * Modified by My Creative Engineering <https://my-creative-engineering.blogspot.com>
- */
 
 // Fill-in information from your Blynk Template here
 //#define BLYNK_TEMPLATE_ID           "TMPLxxxxxx"
